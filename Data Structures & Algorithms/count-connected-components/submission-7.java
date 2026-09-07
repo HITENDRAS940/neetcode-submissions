c9d36bf95c9d81bdad4abdba25e183cd8c55240f
Hitendra Singh Shaktawat <146222582+HITENDRAS940@users.noreply.github.com>
2026-09-07 21:29:45 +0530

    Add: count-connected-components - submission-7

---
+++ Data Structures & Algorithms/count-connected-components/submission-7.java
@@ -0,0 +1,57 @@
+class DSU {
+    private int[] parent;
+    private int[] rank;
+
+    public DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for(int i=0;i<n;i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int i) {
+        if(i==parent[i])
+            return i;
+
+        return parent[i] = find(parent[i]);
+    }
+    
+    public boolean union(int x, int y) {
+        int x_p = find(x);
+        int y_p = find(y);
+
+        if(x_p == y_p) {
+            return false;
+        }
+
+        int rankx = rank[x_p];
+        int ranky = rank[y_p];
+
+        if(rankx > ranky) {
+            parent[y_p] = x_p;
+        } else if(rankx < ranky) {
+            parent[x_p] = y_p;
+        } else {
+            parent[x_p] = y_p;
+            rank[y_p]++;
+        }
+
+        return true;
+    }
+}
+
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+
+        DSU dsu = new DSU(n);
+
+        for(int[] edge : edges) {
+            if(dsu.union(edge[0], edge[1]))
+                n--;
+        }
+
+        return n;
+
+    }
+}
